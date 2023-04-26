56c6c480008870110628c8d542bedcaba4c7fb10
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-26 12:20:47 -0300

    Update visual_prompt.py

--- visual_prompt.py
+++ visual_prompt.py
@@ -3,28 +3,27 @@ import torch
 import torchvision.transforms as transforms
 from PIL import Image
 
-# Load the pre-trained GAN model
-model_path = 'path/to/pretrained_model.pth'
-model = torch.load(model_path, map_location=torch.device('cpu'))
+# Load the pre-trained StyleGAN2 model from GitHub
+model = torch.hub.load('facebookresearch/pytorch_GAN_zoo:hub', 'stylegan2', pretrained=True)
 
 # Define a function to generate an image from a user prompt
 def generate_image(prompt):
     # Convert the prompt to a tensor
-    prompt_tensor = transforms.ToTensor()(prompt)
-    # Generate the image using the GAN model
+    prompt_tensor = transforms.ToTensor()(prompt).unsqueeze(0)
+    # Generate the image using the StyleGAN2 model
     with torch.no_grad():
-        image = model(prompt_tensor)
+        image = model(prompt_tensor, truncation=0.7, truncation_latent=None, input_is_latent=False)
     # Convert the image tensor to a PIL image
-    image = transforms.ToPILImage()(image)
+    image = transforms.ToPILImage()(image.squeeze(0))
     return image
 
 # Define the Streamlit app
 def app():
-    st.title('Text to Image Generator')
+    st.title('Image Generator')
     # Allow the user to input a text prompt
     prompt = st.text_input('Enter a text prompt')
     if prompt:
-        # Generate the image from the user prompt
+        # Generate the image from the text prompt
         image = generate_image(prompt)
         # Display the generated image
         st.image(image, caption='Generated Image', use_column_width=True)
